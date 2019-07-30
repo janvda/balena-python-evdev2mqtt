6b54e6e97cce9dff242b85e18e0d5e3e1cdc3982
janvda <jan@Jans-MacBook-Pro.local>
2019-07-30 09:32:10 +0200

    improved logging

--- keypad2mqtt/main.py
+++ keypad2mqtt/main.py
@@ -3,7 +3,8 @@ import evdev, time
 
 if __name__ == '__main__':
     devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+    print("All input devices:")
     for device in devices:
-        print(device.path, device.name, device.phys)
+        print(" parth=[", device.path,"] name=[", device.name, "] phys=[",device.phys,"]")
     print("sleeping 1 hour...")
     time.sleep(3600)
