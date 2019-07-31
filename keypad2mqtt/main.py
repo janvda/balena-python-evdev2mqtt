31ee216e41e4d9c99352f23007c31566c07875cb
janvda <jan@Jans-MacBook-Pro.local>
2019-07-31 14:36:55 +0200

    added event loop

--- keypad2mqtt/main.py
+++ keypad2mqtt/main.py
@@ -1,10 +1,18 @@
 #empty file
-import evdev, time
+import evdev, time, os
 
 if __name__ == '__main__':
     devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
     print("All input devices:")
     for device in devices:
         print(" path=[", device.path,"], name=[", device.name, "], phys=[",device.phys,"]",sep='')
+    if "input_device" in os.environ:
+        device = evdev.InputDevice(os.environ["input_device"])
+        print("monitoring input:" + device)
+        for event in device.read_loop():
+            if event.type == evdev.ecodes.EV_KEY:
+                print(evdev.categorize(event))
+    else:
+        print("Environment variable input_device is not set !")
     print("sleeping 1 day...")
     time.sleep(3600*24)
