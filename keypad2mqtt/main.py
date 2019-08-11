163776dc01152a9c531b93e4b07b1031c8729481
janvda <jan@Jans-MBP.lan>
2019-08-11 10:48:12 +0200

    using assertions

--- keypad2mqtt/main.py
+++ keypad2mqtt/main.py
@@ -3,16 +3,19 @@ import evdev, time, os
 import paho.mqtt.client as mqtt
 
 if __name__ == '__main__':
-    devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-    print("All connected input devices:")
-    device_cnt=0
-    for device in devices:
-        device_cnt += 1
-        print(" ", device_cnt , ": path=[", device.path,"], name=[", device.name, "], phys=[",device.phys,"]",sep='')
-    if device_cnt == 0 :
-        print("ERROR: no USB input devices connected to the (raspberry pi) device")
-    
-    if "input_device" in os.environ:
+    try:
+        devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+        print("All connected input devices:")
+        device_cnt=0
+        for device in devices:
+            device_cnt += 1
+            print(" ", device_cnt , ": path=[", device.path,"], name=[", device.name, "], phys=[",device.phys,"]",sep='')
+        assert (device_cnt>0),\
+               "ERROR: no USB input devices connected to the (raspberry pi) device"
+
+        assert (os.environ.get("input_device") is not None), \
+               "ERROR: Environment variable \"input_device\" is not set !"
+
         device = evdev.InputDevice(os.environ["input_device"])
         print("\nListening to ",end='')
         print(device)
@@ -20,8 +23,8 @@ if __name__ == '__main__':
         for event in device.read_loop():
             if event.type == evdev.ecodes.EV_KEY:
                 print(evdev.categorize(event))
-    else:
-        print("ERROR: Environment variable \"input_device\" is not set !")
-            
+    except AssertionError as error:
+        print(error)
+
     print("sleeping 1 day...")
     time.sleep(3600*24)
