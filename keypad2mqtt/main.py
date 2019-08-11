29da120ad978cf392bf253df4b2ad88bb13c4f5d
janvda <jan@Jans-MacBook-Pro.local>
2019-08-11 10:32:59 +0200

    improved logging

--- keypad2mqtt/main.py
+++ keypad2mqtt/main.py
@@ -9,15 +9,19 @@ if __name__ == '__main__':
     for device in devices:
         device_cnt += 1
         print(" ", device_cnt , ": path=[", device.path,"], name=[", device.name, "], phys=[",device.phys,"]",sep='')
+    if device_cnt == 0 :
+        print("ERROR: no USB input devices connected to the (raspberry pi) device")
+    
     if "input_device" in os.environ:
         device = evdev.InputDevice(os.environ["input_device"])
-        print("\nListening to device:",end='\n  ')
+        print("\nListening to ",end='')
         print(device)
         device.capabilities(verbose=True)
         for event in device.read_loop():
             if event.type == evdev.ecodes.EV_KEY:
                 print(evdev.categorize(event))
     else:
-        print("Environment variable input_device is not set !")
+        print("ERROR: Environment variable \"input_device\" is not set !")
+            
     print("sleeping 1 day...")
     time.sleep(3600*24)
