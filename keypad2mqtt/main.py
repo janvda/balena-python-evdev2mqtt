9dca5e63c152535953feb4d9a6c9ad1e8b227233
janvda <jan@Jans-MBP.lan>
2019-08-13 19:04:45 +0200

    improved logging

--- keypad2mqtt/main.py
+++ keypad2mqtt/main.py
@@ -18,12 +18,14 @@ if __name__ == '__main__':
         
         try:
             device = evdev.InputDevice(os.environ["input_device"])
+            print("The selected input device (through environment variable \"input_device\"):")
+            print("  ",device)
         except Exception as error:
             print("ERROR: opening input_device (=\"",os.environ["input_device"], "\") failed",sep='')
             print("       Environment variable \"input_device\" must be equal to the path of one of the connected devices (see above)")
             raise
 
-        print("The capabilities of the ",device)
+        print("The capabilities of the selected input device:")
         print(device.capabilities(verbose=True))
 
         print("\nListening to the device ...")
