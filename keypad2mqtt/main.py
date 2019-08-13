4de6678d37b5b16b581e682476af57731903a94d
janvda <jan@Jans-MacBook-Pro.local>
2019-08-13 09:16:46 +0200

    checking opening device

--- keypad2mqtt/main.py
+++ keypad2mqtt/main.py
@@ -5,7 +5,7 @@ import paho.mqtt.client as mqtt
 if __name__ == '__main__':
     try:
         devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
-        print("All connected input devices:")
+        print("All connected USB input devices:")
         device_cnt=0
         for device in devices:
             device_cnt += 1
@@ -15,15 +15,21 @@ if __name__ == '__main__':
 
         assert (os.environ.get("input_device") is not None), \
                "ERROR: Environment variable \"input_device\" is not set !"
+        
+        try:
+            device = evdev.InputDevice(os.environ["input_device"])
+        except Exception as error:
+            print("ERROR: opening $input_device(=\"",os.environ["input_device"], "\") failed",sep='')
+            print("$input_device must be equal to the path of one of the connected devices (see above)")
+            raise
 
-        device = evdev.InputDevice(os.environ["input_device"])
         print("\nListening to ",end='')
         print(device)
         device.capabilities(verbose=True)
         for event in device.read_loop():
             if event.type == evdev.ecodes.EV_KEY:
                 print(evdev.categorize(event))
-    except AssertionError as error:
+    except Exception as error:
         print(error)
 
     print("sleeping 1 day...")
